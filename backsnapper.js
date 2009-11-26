0ef1dfea4e85a4d0cc7319c6f14e37f54b818e42
Nicolas Ward <ultranurd@yahoo.com>
2009-11-25 21:14:55 -0500

    slightly improved logging, cleaned up for experimentation

--- backsnapper.js
+++ backsnapper.js
@@ -1,17 +1,16 @@
 // Snap back to the first page in the history that's not about:blank
 function snapBack(request, sender, sendResponse) {
   // Determine how far back we need to snap back
-  var firstHistoryIndex = 1 - history.length;
-  console.log("snapping back to " + firstHistoryIndex);
+  var historyIndex = 1 - history.length;
 
   // Check if we need to bother snapping back
-  if (firstHistoryIndex < 0) {
-    // Jump back to the beginning
-    history.go(firstHistoryIndex);
+  if (historyIndex < 0) {
+    // Snap back to the beginning
+    history.go(historyIndex);
   }
 
-  // Done; empty ACK
-  sendResponse({});
+  // Done; return snapped-to index and URL
+  sendResponse({"index":historyIndex});
 }
 
 // Add a handler for when this code is called by the extension background page
